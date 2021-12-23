d8118dd5dc99514f08577e9b37575c06a1926c93
Rajkumar9654 <rajkumarcoolx@gmail.com>
2021-12-23 15:45:36 +0530

    currencies

---
+++ src/config/api.js
@@ -0,0 +1,16 @@
+
+const CoinList = (currency) =>
+`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+
+const SingleCoin = (id) =>
+`https://api.coingecko.com/api/v3/coins/${id}`;
+
+const HistoricalChart = (id, days = 365, currency) =>
+`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+
+const TrendingCoins = (currency) =>
+`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+const Currencies = () =>
+`https://api.coingecko.com/api/v3/simple/supported_vs_currencies`;
+
+export {CoinList,SingleCoin,HistoricalChart,TrendingCoins,Currencies}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/globals.css'
 import { Layout } from '@/src/components/'
+import { QueryClient, QueryClientProvider } from "react-query";
+
 function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
+
   )
 }
 
