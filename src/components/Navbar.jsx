7b5b82c79621bdfaa1c65110ba119ac6b4808ec6
Rajkumar9654 <rajkumarcoolx@gmail.com>
2021-12-23 11:24:21 +0530

    nav color change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 function Navbar({ setsidebar, sidebar }) {
     return (
-        <nav className="h-16 flex items-center px-4 justify-end  sticky top-0 bg-zinc-900">
+        <nav className="h-16 flex items-center px-4 justify-end  sticky top-0 bg-gray-900">
             {
                 sidebar &&
                 <div className="h-screen w-screen bg-gray-900 opacity-75 inset-0 fixed lg:hidden" onClick={() => { setsidebar(false) }}></div>
