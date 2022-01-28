1675063afdc23785b7c2a7982a6f3df1a15f67fa
Rajkumar9654 <rajkumarcoolx@gmail.com>
2022-01-28 11:36:27 +0530

    watchlist

--- src/components/view/WatchList.jsx
+++ src/components/view/WatchList.jsx
@@ -16,8 +16,12 @@ function WatchList() {
     useEffect(() => {
         const coins = localStorage.getItem("coins")
         axios.get(CoinList(currency)).then(({ data }) => {
-            const wishlist = data.filter(data => coins.includes(data.id))
-            setwishitem(wishlist)
+            const wishlist = data.filter(data => coins?.includes(data.id))
+            if (wishlist.length === 0 || wishlist == null) {
+                setwishitem([])
+            } else {
+                setwishitem(wishlist)
+            }
         })
     }, [currency]);
     useEffect(() => {
@@ -33,64 +37,79 @@ function WatchList() {
             {
                 wishitem ?
                     <>
-                        <h1 className='sm:text-4xl z-10 mb-2 md:text-5xl font-bold font-mono text-xl'>Watchlist</h1>
-                        <input type="text" value={Name} onChange={(e) => { setName(e.target.value) }} placeholder='Search with name' className='border-2 mb-4  sticky top-20 text-gray-500 border-slate-900  w-full outline-none rounded-lg px-3 py-2 ' />
-                        <div className='mb-4 overflow-x-auto' >
-                            <table className=' w-full ' style={{ minWidth: "500px" }} >
-                                <thead className='bg-blue-500 rounded-full py-4'>
-                                    <tr>
-                                        <th className='text-left p-3 '>Coin
-                                        </th>
-                                        <th className='text-right p-3'>Price
-                                        </th>
-                                        <th className='text-right p-3'> 24h Change
-                                        </th>
-                                        <th className='text-right p-3'> Market Cap
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        wishitem &&
-                                        wishitem.filter((data) => {
-                                            if (Name == "") {
-                                                return data
-                                            } else {
-                                                return data.name.toLowerCase().includes(Name.toLowerCase())
-                                            }
-                                        }).map((data, index) => {
-                                            const { name, id, symbol, total_supply, price_change_percentage_24h, image, current_price } = data
-                                            return (
-                                                <tr key={index} className=' hover:bg-gray-800 duration-300 '>
-                                                    <td className=''>
-                                                        <Link href={`/cryptocurrencies/${id}`} ><a className='text-left flex gap-x-2 p-2'>
-                                                            <img src={image} alt="" className='h-12 w-12 rounded-full' />
-                                                            <div className='flex flex-col'>
-                                                                <span> {symbol.toUpperCase()}</span>
-                                                                <span className='text-sm'>{name}</span>
-                                                            </div>
-                                                        </a></Link>
-                                                    </td>
-                                                    <td className='text-right'> <Link href={`/cryptocurrencies/${id}`}><a >
-                                                        {current_price ?? "Nothing"} {currency.toUpperCase()}
-                                                    </a></Link>
-                                                    </td>
-                                                    <td className='text-right'>      <Link href={`/cryptocurrencies/${id}`}><a >
-                                                        {price_change_percentage_24h > 0 ? <span className='text-green-500'>{Math.floor(price_change_percentage_24h * 1000) / 1000} %</span> : <span className='text-red-500'>{Math.floor(price_change_percentage_24h * 1000) / 1000} %</span>}
-                                                    </a></Link>
-                                                    </td>
-                                                    <td className='text-right p-2'>  <Link href={`/cryptocurrencies/${id}`}><a >
-                                                        {total_supply ?? "Don't know"}
-                                                    </a></Link>
-                                                    </td>
+                        {
+                            wishitem.length == 0 ?
+                                <h1 className=' z-10 pt-10 mb-2 text-gray-300  font-bold font-Space text-2xl'>
+                                    <span>
+                                        YOUR WATCHLIST IS
+                                    </span>
+                                    <br />
+                                    <span className='sm:text-8xl text-6xl'>
+                                        CURRENTLY <br />
+                                        EMPTY
+                                    </span>
+                                </h1>
+                                :
+                                <>
+                                    <h1 className='sm:text-4xl z-10 mb-2 md:text-5xl font-bold font-mono text-xl'>Watchlist</h1>
+                                    <input type="text" value={Name} onChange={(e) => { setName(e.target.value) }} placeholder='Search with name' className='border-2 mb-4  sticky top-20 text-gray-500 border-slate-900  w-full outline-none rounded-lg px-3 py-2 ' />
+                                    <div className='mb-4 overflow-x-auto' >
+                                        <table className=' w-full ' style={{ minWidth: "500px" }} >
+                                            <thead className='bg-blue-500 rounded-full py-4'>
+                                                <tr>
+                                                    <th className='text-left p-3 '>Coin
+                                                    </th>
+                                                    <th className='text-right p-3'>Price
+                                                    </th>
+                                                    <th className='text-right p-3'> 24h Change
+                                                    </th>
+                                                    <th className='text-right p-3'> Market Cap
+                                                    </th>
                                                 </tr>
-                                            )
-                                        })
-                                    }
+                                            </thead>
+                                            <tbody>
+                                                {
+                                                    wishitem.filter((data) => {
+                                                        if (Name == "") {
+                                                            return data
+                                                        } else {
+                                                            return data.name.toLowerCase().includes(Name.toLowerCase())
+                                                        }
+                                                    }).map((data, index) => {
+                                                        const { name, id, symbol, total_supply, price_change_percentage_24h, image, current_price } = data
+                                                        return (
+                                                            <tr key={index} className=' hover:bg-gray-800 duration-300 '>
+                                                                <td className=''>
+                                                                    <Link href={`/cryptocurrencies/${id}`} ><a className='text-left flex gap-x-2 p-2'>
+                                                                        <img src={image} alt="" className='h-12 w-12 rounded-full' />
+                                                                        <div className='flex flex-col'>
+                                                                            <span> {symbol.toUpperCase()}</span>
+                                                                            <span className='text-sm'>{name}</span>
+                                                                        </div>
+                                                                    </a></Link>
+                                                                </td>
+                                                                <td className='text-right'> <Link href={`/cryptocurrencies/${id}`}><a >
+                                                                    {current_price ?? "Nothing"} {currency.toUpperCase()}
+                                                                </a></Link>
+                                                                </td>
+                                                                <td className='text-right'>      <Link href={`/cryptocurrencies/${id}`}><a >
+                                                                    {price_change_percentage_24h > 0 ? <span className='text-green-500'>{Math.floor(price_change_percentage_24h * 1000) / 1000} %</span> : <span className='text-red-500'>{Math.floor(price_change_percentage_24h * 1000) / 1000} %</span>}
+                                                                </a></Link>
+                                                                </td>
+                                                                <td className='text-right p-2'>  <Link href={`/cryptocurrencies/${id}`}><a >
+                                                                    {total_supply ?? "Don't know"}
+                                                                </a></Link>
+                                                                </td>
+                                                            </tr>
+                                                        )
+                                                    })
+                                                }
 
-                                </tbody>
-                            </table>
-                        </div>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </>
+                        }
                     </>
                     :
                     <>
