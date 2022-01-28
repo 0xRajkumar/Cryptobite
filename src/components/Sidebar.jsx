6f3df506bee7c6b33444913355c1bf86dcf3c465
Rajkumar9654 <rajkumarcoolx@gmail.com>
2022-01-28 11:38:23 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ function Sidebar({ setsidebar, sidebar }) {
     }, [pathname])
     return (
         <aside className={`${sidebar ? "left-0" : "-left-96"} z-10 w-64 lg:static fixed bg-gray-900 h-screen  duration-500`}>
-            <Link href='/'><a className="flex flex-row items-center justify-center gap-x-2 h-16">
+            <Link href='/'><a onClick={() => { setsidebar(false) }} className="flex flex-row items-center justify-center gap-x-2 h-16">
                 <Image src="/cryptocurrency.png" alt="ok" height={42} width={42} />
                 <h1 className="font-semibold  text-2xl">
                     CRYPTOBITE
